882e8fd30bc14b6d76023486d40939fccd10053e
Sai Srikanth <saisrikanth2007@gmail.com>
2025-11-22 06:33:34 +0000

    fix: deployment config and workflow

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== THIS IS CRITICAL. If missing, you get a 404.
+  output: "export",  // <=== THIS IS MANDATORY FOR GITHUB PAGES
   images: {
-    unoptimized: true,
+    unoptimized: true, // <=== THIS IS MANDATORY TO PREVENT BUILD ERRORS
   },
 };
 
